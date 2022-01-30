10e321b8d0cc0170303e3fb33d92b3b0c3dfff65
samithakalpana <66740752+samithakalpana@users.noreply.github.com>
2022-01-30 13:54:04 +0530

    MAP CHANGES 1

--- contact-us.php
+++ contact-us.php
@@ -69,7 +69,7 @@
                 <br>
                 <br>
                 <h2 style="text-align: center;">Show the client's location</h2>
-                <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7060970.521913474!2d-102.60272453381052!3d37.588245893295074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited+States!5e0!3m2!1sen!2sin!4v1495005824042" allowfullscreen></iframe> -->
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7060970.521913474!2d-102.60272453381052!3d37.588245893295074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited+States!5e0!3m2!1sen!2sin!4v1495005824042" allowfullscreen></iframe>
             </div>
             <div class="contact_wrap">
                 <div class="row">
