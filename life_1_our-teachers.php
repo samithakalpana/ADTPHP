2ca3520a6a7e60e759ee65e84e0f027d196140f4
Y.K.Pasindu Harshana <pasinduharshana34@gmail.com>
2022-02-06 22:04:29 +0530

    add life story pages
    5 pages of life story battle and legacy using our teachrs page

---
+++ life_1_our-teachers.php
@@ -0,0 +1,138 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+
+<!-- Mirrored from primehostingindia.com/templatemonster/html/templezen/our-teachers.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 06 Jan 2022 18:06:09 GMT -->
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<title>Templezen - Buddhist Temple HTML5 Template</title>
+	<!--Bootstrap -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+	<!--OWL Carousel slider-->
+	<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+	<!--Custome Style -->
+	<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+	<!--Responsive Style -->
+	<link rel="stylesheet" href="assets/css/responsive.css" type="text/css">
+	<!--magnific Style -->
+	<link rel="stylesheet" href="assets/css/magnific-popup.css" type="text/css">
+	<!--FontAwesome Font Style -->
+	<link href="assets/css/font-awesome.css" rel="stylesheet">
+	<!-- Fav and touch icons -->
+	<link rel="shortcut icon" href="favicon.png">
+	<!-- Google-Font-->
+	<link href="https://fonts.googleapis.com/css?family=Marcellus&amp;display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i&amp;display=swap" rel="stylesheet"><!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body id="tl">
+		<!-- Header -->
+		<?php include_once 'header.php'; ?>
+	<!-- /Header -->
+
+	<!-- Inner-intro -->
+	<section id="inner_intro" class="section-padding">
+		<div class="container">
+			<div class="inner_wp z_index white_text">
+				<div class="row">
+					<div class="col-md-12">
+						<h1 class="text-center">Life of Anagarika Dharmapala</h1>
+						<h5 class="text-center">(17th of September 1864 – 29th of April 1933)</h5>
+						<nav class="breadcrumb">
+							<ul>
+								<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+								<li class="breadcrumb-item active">Don David Hewavitarne</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- /Inner-intro -->
+
+	<section class="sa-paster-about-section section-padding pd-default">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-7">
+						<div class="sa-paster-about-area">
+							<!-- <h3>Gopal dhanam</h3> -->
+							<h3>Don David Hewavitarne</h3>
+							<!-- <p class="designation">Yoga Teacher</p> -->
+							<p class="designation">(From 1864-1885)</p>
+							<p>	Don David Hewavitarne, who would become known throughout the world in the early twentieth century as Anagarika Dharmapala, was born to Mallika and Don Carolis Hewavitarne on the 17th of September 1864. 
+							
+							</p>
+							<p>							
+							
+At that time the Hewavitarne family was living in Pettah, then a residential part of then Ceylon's (now Sri Lanka) capital city, but would soon move to adjoining Kotahena, a salubrious neighborhood of homes with sprawling gardens and tree-lined roads. His birth was registered at St Benedicts Church of Kotahena, as was a requirement at the time.
+	</p><p>
+David’s primary education is marked by the many different schools he attended. These included the Colombo Girl’s Infant Home, Pettah Catholic School (later St Mary’s), Baptist Sinhala School, St Benedicts College, CMS Boys School in Kotte, St Thomas’ Collegiate Institute and Colombo Academy (later Royal College). Despite the many upheavals to his schooling, his parents gave him a firm foundation in Buddhism and in the local tongue, Sinhala. He learnt written Sinhala at a very early age under a well-known Sinhala scholar of the time, Haramanis Gurunanse, and his parents inculcated in him a devotion to the Buddha, the Dhamma (Doctrine) and the Sangha (Clergy) - 'the Triple Gem', the three pillars of Buddhism.
+In later years he says, “In school was fearless, daring and had a kind of aristocratic hauteur and did not care about anybody. At home I had the same spirit and what I wanted I got. I was stubborn, resolute and could not bear any kind of intolerance. I had no love for worldly things. I loved simplicity, solitude and any form of helping the poor, even to the extent of giving everything I had…”. 
+He writes about his beloved parents with much love and respect, although he criticizes them occasionally: his mother for her detachment and his father for his sense of discipline. He says he owes his understanding of the Dhamma and the law of impermanence to his mother, and that these gave him the foundation to renounce his lay life. He says it was his father who introduced him to the vow of Brahmacharya (pure conduct) at the age of nine years and constantly advised him not to “depend on worldly things”.
+	</p><p>
+At an early age, Don David was drawn to spiritual ideas, meditation and serving the poor. It is easy to see the young man being attracted to the Theosophist Movement. He was introduced to it by Ven Migettuwatte Gunananda Thero and began reading the Theosophist from its first publication in 1878.  When Col Henry Steel Olcott, an American and Madam Helena Blavatsky, of Russian origin (two of the main founders of the Theosophical Society), arrived in Ceylon in 1880, young David attended Olcott’s lectures and followed up by reading voraciously about spiritual development. He had a desire to be a renunciant and also at this time became a vegetarian. He joined the Theosophical Society in 1884. During these formative years, he also worked at the Department of Public Instruction in 1885. Yet, his interests were already firmly turned towards spiritualism and service.
+	</p><p>
+The family setting David grew up in, the beautiful garden and the large house of his childhood and parents who understood the practice of the Dhamma and wholeheartedly encouraged it in their son,  laid a spiritual foundation in his life. Spiritual progress and the practice of the Buddha’s teachings became the foundational purpose of his life. Everything else he engaged in would emanate from his fervent dedication to this purpose.
+	</p>
+	<p>
+At the age of 21 years David left the luxuries and conveniences of his family home (the Hewavitarnes had by then moved to a sprawling house and gardens at Aloe Avenue in Kollupitiya) with the blessings of his parents to take up the life of a renunciant.
+
+							
+							</p>
+							<!-- <div class="sa-team-social">
+								<h5>Social links</h5>
+								<ul class="text-center">
+									<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li><a href="#"><i class="fab fa-youtube"></i></a></li>
+									<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+								</ul>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-xl-4 col-lg-5 offset-xl-1 align-self-center">
+						<div class="ss-pastor-img-wrap">
+							<!-- <img src="assets/images/pastor-1.jpg" alt="img"> -->
+							<img src="assets/images/pastor-1.jpg" alt="img">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	<!-- Footer -->
+	<?php include_once 'footer.php';?>
+	<!-- /Footer -->
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<!--Magnific-Popup-JS-->
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<!-- Countdown -->
+	<script src="assets/js/jquery.countdown.min.js"></script>
+	<!--Custome-JS-->
+	<script src="assets/js/interface.js"></script>
+	<!--Carousel-JS-->
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<!--ion-range-slider-JS-->
+	<script src="assets/js/ion.rangeSlider.min.js"></script>
+	<!--Audio-JS-->
+	<script src="assets/js/audio_custome.js"></script>
+</body>
+
+
+<!-- Mirrored from primehostingindia.com/templatemonster/html/templezen/our-teachers.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 06 Jan 2022 18:06:34 GMT -->
+</html>
