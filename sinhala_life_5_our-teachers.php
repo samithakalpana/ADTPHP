766d32f737635f049096aa60fefd652deab9c41b
samithakalpana <66740752+samithakalpana@users.noreply.github.com>
2022-03-03 22:43:18 +0530

    Pasindu (#23)
    * Update style.css
    
    * add empty sinhala life pages
    
    Co-authored-by: Y.K.Pasindu Harshana <pasinduharshana34@gmail.com>

---
+++ sinhala_life_5_our-teachers.php
@@ -0,0 +1,148 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+
+<!-- Mirrored from primehostingindia.com/templatemonster/html/templezen/our-teachers.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 06 Jan 2022 18:06:09 GMT -->
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<title>ADT-Life Of Anagarika Dharmapala</title>
+	<!--Bootstrap -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+	<!--OWL Carousel slider-->
+	<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+	<!--Custome Style -->
+	<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+	<!--Responsive Style -->
+	<link rel="stylesheet" href="assets/css/responsive.css" type="text/css">
+	<!--magnific Style -->
+	<link rel="stylesheet" href="assets/css/magnific-popup.css" type="text/css">
+	<!--FontAwesome Font Style -->
+	<link href="assets/css/font-awesome.css" rel="stylesheet">
+	<!-- Fav and touch icons -->
+	<link rel="shortcut icon" href="favicon.png">
+	<!-- Google-Font-->
+	<link href="https://fonts.googleapis.com/css?family=Marcellus&amp;display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i&amp;display=swap" rel="stylesheet"><!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body id="tl">
+		<!-- Header -->
+		<?php include_once 'header.php'; ?>
+	<!-- /Header -->
+
+	<!-- Inner-intro -->
+	<section id="inner_intro" class="section-padding">
+		<div class="container">
+			<div class="inner_wp z_index white_text">
+				<div class="row">
+					<div class="col-md-12">
+						<h1 class="text-center">Life of Anagarika Dharmapala</h1>
+						<h5 class="text-center">(17th of September 1864 – 29th of April 1933)</h5>
+						<nav class="breadcrumb">
+							<ul>
+								<li class="breadcrumb-item"><a href="index.php">Home</a></li>
+								<li class="breadcrumb-item active">The Legacy</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- /Inner-intro -->
+
+	<section class="sa-paster-about-section section-padding pd-default">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-7">
+						<div class="sa-paster-about-area">
+							<!-- <h3>Gopal dhanam</h3> -->
+							<h3>The Battle for Buddha Gaya</h3>
+							<!-- <p class="designation">Yoga Teacher</p> -->
+							<p class="designation"></p>
+							<p>	
+							Anagarika Dharmapala’s life as an activist, missionary, freedom fighter, social reformer and renunciant was remarkable as it was extraordinary. 
+							</p><p>
+							He managed to spread the word of the Buddha in the West on a scale that had not been attempted before, at a time when colonial powers and their socio-religious ideas were dominating the world. He used the existing tropes and frameworks of the colonizers to revive Buddhism in the public consciousness. He understood that the revival needed to take place within the milieu of his times and used the same tools of the rulers. With Col Olcott, where there were Christian Sunday schools he offered people Buddhist Sunday schools, he spearheaded schools of the same standard for Buddhist children as those the Christian missionaries had founded, and where locals were ashamed or embarrassed of native dress and customs, he urged them to change their mindset. Most importantly, he revived a self-consciousness in Buddhists of their own worth and the uniqueness of the Buddha’s teachings.
+							</p>	<p>
+							An internationalist with a global viewpoint, he thereby founded a vision for a world community of Buddhists with the Maha Bodhi Society he founded. The organization became a banner, the earliest representative body of all Buddhists, and the earliest unifying banner for all Buddhists from the different continents.
+							</p><p>
+							He travelled extensively, at a time when travel was not easy, enduring physical pain and hardship, not only spreading his vision and message, but also visiting the premier institutions of his time in America and Europe to study the latest ideas on education and industrial training. As a result, he met and inter-acted with major thinkers and activists of his day. They ranged from Swami Vivekananda, Tagore and Gandhi in India, to royalty and leaders in Rangoon, Siam, Japan, to scholars such as Rhys Davis and Sir Edwin Arnold in England and educationists such as William Rainey Harper, the founding president of the University of Chicago in America.  He was quick to grasp the value of the latest innovations in technology, for communication, healthcare and production. He understood the importance of the use of publications, translations, education and most importantly, dialogue between societies and within societies, for his mission.
+Anagarika Dharmapala’s multifaceted nature and work gave rise to multiple narratives, and left room for a few in contemporary society to distort his vision and take his rhetoric out of context, in many instances without verifying against primary sources. Some have used his words to advocate racism, ethnic violence, political activism of Buddhist clergy and divisive nationalisms, pointing to Anagarika Dharmapala, his speeches and his writings. They have, by default, or by design, missed the ‘woods for the trees’.
+
+							</p>	<p>
+
+							The Anagarika Dharmapala Trust does not condone or support the use of his words that are not cited by credible sources, that are misquoted, misreported, taken in parts and out of context and/or incorrectly paraphrased and, in particular, used to incite racial and religious hatred.
+The legacy Anagarika Dharmapala wanted to leave to the future generations of Sri Lanka and the world is clear in the objects of the Trust he created, bearing his name, to continue his work of a lifetime. 
+
+							</p><p>
+							Anagarika Dharmapala envisioned a world where there would be widespread knowledge of the Buddha’s teaching, with people who dedicated their lives to that mission by living as renunciants and, the upliftment of under privileged people through better healthcare, education and vocational training. 
+							</p>	<p>
+							That was the legacy Angarika Dharmapala wished to leave to the future generations.
+							</p>				
+							
+
+
+
+
+
+
+
+
+
+							<!-- <div class="sa-team-social">
+								<h5>Social links</h5>
+								<ul class="text-center">
+									<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li><a href="#"><i class="fab fa-youtube"></i></a></li>
+									<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+								</ul>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-xl-4 col-lg-5 offset-xl-1 align-self-center">
+						<div class="ss-pastor-img-wrap">
+							<!-- <img src="assets/images/pastor-1.jpg" alt="img"> -->
+							<img src="assets/images/pastor-1.jpg" alt="img">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	<!-- Footer -->
+	<?php include_once 'footer.php';?>
+	<!-- /Footer -->
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<!--Magnific-Popup-JS-->
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<!-- Countdown -->
+	<script src="assets/js/jquery.countdown.min.js"></script>
+	<!--Custome-JS-->
+	<script src="assets/js/interface.js"></script>
+	<!--Carousel-JS-->
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<!--ion-range-slider-JS-->
+	<script src="assets/js/ion.rangeSlider.min.js"></script>
+	<!--Audio-JS-->
+	<script src="assets/js/audio_custome.js"></script>
+</body>
+
+
+<!-- Mirrored from primehostingindia.com/templatemonster/html/templezen/our-teachers.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 06 Jan 2022 18:06:34 GMT -->
+</html>
