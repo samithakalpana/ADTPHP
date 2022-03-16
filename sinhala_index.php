fff0a995b9432612022ccb1864d6d67815c62dfa
samithakalpana <66740752+samithakalpana@users.noreply.github.com>
2022-03-16 21:34:22 +0530

    minor changes in text
    minor changes in text

--- sinhala_index.php
+++ sinhala_index.php
@@ -21,7 +21,7 @@ ob_start();
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<meta name="keywords" content="">
 	<meta name="description" content="">
-	<title>Templezen - Buddhist Temple HTML5 Template</title>
+	<title>ADT</title>
 	<!--Bootstrap -->
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
 	<!--OWL Carousel slider-->
